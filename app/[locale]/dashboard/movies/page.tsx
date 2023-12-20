90eacbd8c71e93df7db8df849818b2b8b3a3a40a
Marti Aguilera <mrtxaver@gmail.com>
2023-12-20 20:00:36 +0100

    unstable set request Locale error2

--- app/[locale]/dashboard/movies/page.tsx
+++ app/[locale]/dashboard/movies/page.tsx
@@ -1,7 +1,9 @@
 import {getTranslations} from 'next-intl/server';
- 
-export default async function Index() {
+import {unstable_setRequestLocale} from 'next-intl/server';
+
+export default async function Index({params: {locale}}: {params:{locale: string}}) {
   const t = await getTranslations('LoginRegister');
+  unstable_setRequestLocale(locale);
 
   return <h1>{t('Password')}</h1>;
 }
